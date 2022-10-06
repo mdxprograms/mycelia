1601a21cc44aedc80540e0c0f04651ae725a5548
Josh Waller <mdx.programs@gmail.com>
2022-10-06 10:53:51 -0400

    adds lib configuration

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,21 @@ import { defineConfig } from "vitest/config";
 
 // In vitest.config.js, add the following
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/mycelia.ts"),
+      name: "mycelia",
+      filename: "mycelia",
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {
+          mycelia: "mycelia",
+        },
+      },
+    },
+  },
   test: {
     environment: "happy-dom",
     setupFiles: ["vitest-setup.ts"],
