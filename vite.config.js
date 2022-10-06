0459b86137b90b469a37eaa1fcf156afe38878bb
Josh Waller <mdx.programs@gmail.com>
2022-10-06 11:27:51 -0400

    fixing configs

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,13 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "lib/mycelia.ts"),
       name: "mycelia",
-      // the proper extensions will be added
       fileName: "mycelia",
     },
     test: {
       exclude: configDefaults.exclude,
     },
     rollupOptions: {
+      external: [],
       output: {
         globals: {
           mycelia: "mycelia",
--- vitest.config.js
+++ vitest.config.js
@@ -1,23 +1,7 @@
 // vitest-setup.js
 import { defineConfig } from "vitest/config";
 
-// In vitest.config.js, add the following
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "lib/mycelia.ts"),
-      name: "mycelia",
-      filename: "mycelia",
-    },
-    rollupOptions: {
-      external: [],
-      output: {
-        globals: {
-          mycelia: "mycelia",
-        },
-      },
-    },
-  },
   test: {
     environment: "happy-dom",
     setupFiles: ["vitest-setup.ts"],
