27010453cb043ddd795804914c1ec53a69503655
Josh Waller <mdx.programs@gmail.com>
2023-03-07 17:34:08 -0500

    updating package naming for publishing

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "lib/mycelia.ts"),
-      name: "mycelia",
+      name: "@wallerbuilt/mycelia",
       fileName: "mycelia",
     },
     test: {
